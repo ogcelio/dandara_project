83f5f8cdf64f4c047fe10348058e5f4bbfcb01b5
ogcelio <joaocelioaraujosander@gmail.com>
2026-01-08 12:28:33 -0300

    adding backward sweep func

--- c/DD/calc/sweep.c
+++ c/DD/calc/sweep.c
@@ -1,5 +1,18 @@
+#include <math.h>
+
 void
-calc_foward(int N, int TOTAL_NODES, int NUM_REGS, int NUM_NODES[], int REGS[], double H[], double SS[], double Q[], double SIGMA_T[], double MI[N], double psi[TOTAL_NODES + 1][N])
+calc_foward(
+    int N,
+    int TOTAL_NODES,
+    int NUM_REGS,
+    int NUM_NODES[],
+    int REGS[],
+    double H[],
+    double SS[],
+    double Q[],
+    double SIGMA_T[],
+    double MI[N],
+    double psi[TOTAL_NODES + 1][N])
 {
     int i, j, m, reg, num_nodes;
     int node = 1;
@@ -7,7 +20,6 @@ calc_foward(int N, int TOTAL_NODES, int NUM_REGS, int NUM_NODES[], int REGS[], d
 
     double half_sigma_t;
     double mi_h[HALF_N];
-
 
     for (i = 0; i < NUM_REGS; i++)
     {
@@ -25,9 +37,55 @@ calc_foward(int N, int TOTAL_NODES, int NUM_REGS, int NUM_NODES[], int REGS[], d
         {
             for (m = 0; m < HALF_N; m++)
             {
-                psi[node][m] = ((mi_h[m] - half_sigma_t) * psi[node - 1][m] + SS[node - 1] + Q[reg]) / (mi_h[m] + half_sigma_t);
+                psi[node][m] = ((mi_h[m] - half_sigma_t) * psi[node - 1][m] + SS[node - 1] + Q[reg])
+                                /
+                                (mi_h[m] + half_sigma_t);
             }
         }
     }
+}
 
+void
+calc_backward(
+    int N,
+    int TOTAL_NODES,
+    int NUM_REGS,
+    int NUM_NODES[],
+    int REGS[],
+    double H[],
+    double SS[],
+    double Q[],
+    double SIGMA_T[],
+    double MI[N],
+    double psi[TOTAL_NODES + 1][N])
+{
+    int i, j, m, reg, num_nodes;
+    int node = TOTAL_NODES - 1;
+    int HALF_N = N / 2;
+
+    double half_sigma_t;
+    double mi_h[HALF_N];
+
+    for (i = NUM_REGS; i > 0; i--)
+    {
+        reg = REGS[i] - 1;
+        num_nodes = NUM_NODES[i];
+
+        half_sigma_t = SIGMA_T[reg] / 2;
+
+        for (m = 0; m < HALF_N; m++)
+        {
+            mi_h[m] = MI[m] / H[i];
+        }
+
+        for (j = 0; j < num_nodes; j++, node--)
+        {
+            for (m = HALF_N; m < N; m++)
+            {
+                psi[node][m] = ((fabs(mi_h[m]) - half_sigma_t) * psi[node + 1][m] + SS[node] + Q[reg])
+                                /
+                                (mi_h[m] + half_sigma_t);
+            }
+        }
+    }
 }
