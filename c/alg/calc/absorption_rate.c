5c876e0ccd7e83da7a9a6e79e5f09509b668ca76
ogcelio <joaocelioaraujosander@gmail.com>
2026-01-12 14:14:32 -0300

    adding absorption and escape rate algorithms

---
+++ c/alg/calc/absorption_rate.c
@@ -0,0 +1,29 @@
+void
+calc_abs_rate(
+    int NUM_NODES[],
+    int NUM_REGS,
+    int REGS[NUM_REGS],
+    double AVG_FI[],
+    double H[NUM_REGS],
+    double SIGMA_T[],
+    double SIGMA_S0[],
+    double abs_rate[NUM_REGS])
+{
+    int i, j, num_nodes;
+    int node = 0;
+    double sum, sigma_a;
+
+    for (i = 0; i < NUM_REGS; i++)
+    {
+        sigma_a = SIGMA_T[i] - SIGMA_S0[i];
+
+        num_nodes = NUM_NODES[i];
+        sum = 0;
+        for (j = 0; j < num_nodes; j++, node++)
+        {
+            sum += sigma_a * AVG_FI[node] * H[i];
+        }
+
+        abs_rate[i] = sum;
+    }
+}
---
+++ c/alg/calc/escape_rate.c
@@ -0,0 +1,26 @@
+#include <math.h>
+
+void
+calc_escape_rate(
+    int N,
+    int HALF_N,
+    int TOTAL_NODES,
+    double MI[N],
+    double W[N],
+    double PSI[TOTAL_NODES + 1][N],
+    double escape_rate[2])
+{
+    int n;
+    double sum_0 = 0;
+    double sum_H = 0;
+
+    for (n = 0; n < HALF_N; n++)
+    {
+        sum_0 += fabs(MI[n + HALF_N] * W[n + HALF_N] * PSI[0][n + HALF_N]);
+
+        sum_H += fabs(MI[n] * W[n] * PSI[TOTAL_NODES][n]);
+    }
+
+    escape_rate[0] = sum_0;
+    escape_rate[1] = sum_H;
+}
