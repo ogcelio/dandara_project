f35cad9dcb5c1f446536b7d24aaa73685897790e
ogecelio <joaocelioaraujosander@gmail.com>
2026-03-01 14:24:46 -0300

    adding rust version of medium flux and scattering source

--- linear-rust/src/methods/dd/medium_flux.rs
+++ linear-rust/src/methods/dd/medium_flux.rs
@@ -4,3 +4,10 @@
 
     SANDER, J. C. A. - 2026
 */
+
+fn medium_flux(n: usize, psi: &[f64], psim: &mut [f64]) {
+    psim.iter_mut()
+        .zip(psi)
+        .zip(&psi[n..])
+        .for_each(|((dest, &minus_half), &plus_half)| *dest = 0.5 * (minus_half + plus_half));
+}
--- linear-rust/src/methods/dd/scattering_source.rs
+++ linear-rust/src/methods/dd/scattering_source.rs
@@ -4,3 +4,18 @@
 
     SANDER, J. C. A. - 2026
 */
+
+fn calc_scattering_source(n: usize, w: &[f64], h_sigma_s0: &[f64], psim: &[f64], ss: &mut [f64]) {
+    ss.iter_mut()
+        .zip(psim.chunks_exact(n))
+        .zip(h_sigma_s0.iter())
+        .for_each(|((ss_j, psim_j), &h_s0_j)| {
+            let sum: f64 = w
+                .iter()
+                .zip(psim_j)
+                .map(|(&w_n, &psim_j_n)| w_n * psim_j_n)
+                .sum();
+
+            *ss_j = h_s0_j * sum;
+        });
+}
